3addcf6febc0a9436f4288168072a86ed5aa8b81
ChetaN-Kumar7 <chetankumar3018@gmail.com>
2025-01-26 16:27:18 +0530

    Setup project and Redux store and features

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import NavBar from './NavBar'
+import Body from './Body'
+import Login from './Login'
+import Profile from './Profile'
+import { Provider } from 'react-redux'
+import appStore from './utils/appStore'
+import Feed from './Feed'
+
+function App() {
+
+
+  return (
+    <>
+    <Provider store={appStore}>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/' element={<Body/>}>
+            <Route path='/' element={<Feed/>}></Route>
+            <Route path='/Login' element={<Login/>}></Route>
+            <Route path='/Profile' element={<Profile/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
+    </Provider>
+    </>
+  )
+}
+
+export default App
